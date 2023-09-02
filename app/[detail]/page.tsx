3acb1e7bf867dc27fb1a9483e396fa86bf3654f4
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-02 08:05:26 +0000

    update detail responsive ✅

--- app/[detail]/page.tsx
+++ app/[detail]/page.tsx
@@ -15,7 +15,7 @@ const getData = async({detail}:Params) => {
 const Page = async({params}:{ params: Params }) => {
   const movie = await getData(params)
   return(
-    <div className="flex flex-wrap gap-20 mt-10">
+    <div className="flex flex-wrap gap-20 mt-10 flex-col items-center xl:flex-row xl:items-start">
       <div className="w-full sm:w-[48%] md:w-[300px]">
         <Image 
           src={movie.Poster} 
@@ -30,7 +30,7 @@ const Page = async({params}:{ params: Params }) => {
         />
       </div>
 
-      <div className="w-full sm:w-[48%] lg:w-[500px]">
+      <div className="w-full sm:w-[500px] lg:w-[600px]">
         <h1>{movie.Title}</h1>
         <h3>STORYLINE</h3>
         <p>{movie.Plot}</p>
@@ -59,7 +59,7 @@ const Page = async({params}:{ params: Params }) => {
         
       </div>
 
-      <div className="w-[130px] lg:mt-8">
+      <div className="w-full sm:w-[500px] lg:w-[600px] lg:mt-8 xl:w-[150px]">
         <h3 className="mt-4">Written by</h3>
         <p>{movie.Writer}</p>
       </div>
