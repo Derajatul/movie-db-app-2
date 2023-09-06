d560ffcb9750b5cd4a07907982bc885ad4b2e809
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-06 14:31:01 +0000

    add footer ✅

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return(
+    <footer className="footer footer-center p-4  text-base-content">
+  <aside>
+    <p>Derajatul © {new Date().getFullYear()}</p>
+  </aside>
+</footer>
+  )
+}
+
+export default Footer
