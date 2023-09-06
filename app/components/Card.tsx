7d34274ec1d624b414e53b22f000e13ddde3381b
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-06 14:31:16 +0000

    update card ✅

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -4,34 +4,28 @@ import Link from 'next/link'
 type Card = {
   poster: string;
   title: string;
-  year: string;
-  type: string;
   href: string;
 }
 
-const Card = ({poster,title,year,type,href}:Card) => {
+const Card = ({poster,title,href}:Card) => {
   return(
     <Link 
-      className="w-[150px] sm:w-[48%] lg:w-[31.5%] xl:w-[22%] bg-neutral" 
+      className="w-full sm:w-[48%] lg:w-[31.5%] xl:w-[22%] bg-neutral card-compact" 
       href={href}
       >
-      <figure>
+    
         <Image
           width={500}
           height={500}
           style={{
-            width: '100%',
-            height: 'auto',
+            width:'100%',
+            height: 'auto'
           }}
           src={poster} 
           alt={title} 
         />
-      </figure>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
-          <div className="card-actions">
-            
-          </div>
         </div>
       </Link>
   )
